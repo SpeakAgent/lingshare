569968b68a0d49e090b5b56646922b47a91c2d84
Mal Jones <mjones@forumone.com>
2015-01-24 22:22:30 -0500

    Hover animations added to shell.

--- site/static/js/controllers/memory.js
+++ site/static/js/controllers/memory.js
@@ -76,7 +76,7 @@ mainApp.controller('MemoryController', ['$scope', '$timeout', '$http',
             }
           }
           $http(req).success(function (data) {
-            
+
           })
           .error(function (data) {
 
@@ -150,7 +150,7 @@ mainApp.controller('MemoryController', ['$scope', '$timeout', '$http',
   	 };
 
      $scope.flipCard = function (card) {
-      if ($scope.showCards.indexOf(card.slug) == -1 && 
+      if ($scope.showCards.indexOf(card.slug) == -1 &&
         $scope.showCards.length < 2) {
         $scope.showCards.push(card.slug)
       }
